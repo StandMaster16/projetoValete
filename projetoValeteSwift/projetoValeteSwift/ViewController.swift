4df75ed1100a1c8d60635219df11ff840f37181d
ICMMAC10-78E2 <icmmac10-78e2@ICMMAC10-78E2s-Mac-mini.local>
2022-08-30 17:11:00 -0400

    valeteSwift

---
+++ projetoValeteSwift/projetoValeteSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projetoValeteSwift
+//
+//  Created by ICMMAC10-78E2 on 30/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
