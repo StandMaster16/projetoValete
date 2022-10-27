6ae6b5b0a904a59dba797221609781ffad612241
Matheus Eduardo Amazonas Costa <75271003+StandMaster16@users.noreply.github.com>
2022-10-27 08:40:53 -0400

    Add files via upload

---
+++ projeto_piloto/projeto_piloto/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projeto_piloto
+//
+//  Created by ICMMAC06-7A13 on 26/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ projeto_piloto/projeto_piloto/Controllers/tabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  tabBarViewController.swift
+//  projeto_piloto
+//
+//  Created by ICMMAC06-7A13 on 26/10/22.
+//
+
+import UIKit
+
+class tabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.selectedIndex = 2;
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
